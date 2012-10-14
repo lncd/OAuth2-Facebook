c4274bbc6a2b12ef42bfd0aa412d4364781e505c
Alex Bilbie <alex@alexbilbie.com>
2012-10-14 17:13:24 +0100

    Squashed commit of the following:
    commit f59663814490a14114e1296f5c42ba998fdb5e5a
    Author: Alex Bilbie <alex@alexbilbie.com>
    Date:   Sun Oct 14 17:12:53 2012 +0100
    
        Version bump
    
        Signed-off-by: Alex Bilbie <alex@alexbilbie.com>
    
    commit b70542cda0cfe84bf16b93506a7907c5740e4625
    Author: Alex Bilbie <alex@alexbilbie.com>
    Date:   Sun Oct 14 17:12:26 2012 +0100
    
        Docblock header. Thanks to @philsturgeon
    
        Signed-off-by: Alex Bilbie <alex@alexbilbie.com>
    
    commit 9a39098785a31ed9a633504c92d5aa4376d7f930
    Merge: 1c37f29 94ad161
    Author: Alex Bilbie <alex@alexbilbie.com>
    Date:   Sun Oct 14 16:47:17 2012 +0100
    
        Merge branch 'release/v1'
    
    Signed-off-by: Alex Bilbie <alex@alexbilbie.com>

--- src/FacebookIDP/Facebook.php
+++ src/FacebookIDP/Facebook.php
@@ -1,50 +1,63 @@
 <?php
+/**
+ * Facebook OAuth2 Provider
+ *
+ * This IDP is based on code originally by Phil Sturgeon which can be found here:
+ *
+ *     https://github.com/philsturgeon/codeigniter-oauth2/blob/master/libraries/Provider/Facebook.php
+ *
+ * @package    lncd/oauth2-facebook
+ * @category   Provider
+ * @author     Alex Bilbie
+ * @copyright  (c) 2012 University of Lincoln
+ * @license    http://opensource.org/licenses/mit-license.php
+ */
 namespace FacebookIDP;
 
 class Facebook extends \Oauth2\Client\IDP {
 
-	public $scope = array('email', 'read_stream');
-	public $responseType = 'string';
+    public $scope = array('email', 'read_stream');
+    public $responseType = 'string';
 
-	public function urlAuthorize()
-	{
-		return 'https://www.facebook.com/dialog/oauth';
-	}
+    public function urlAuthorize()
+    {
+        return 'https://www.facebook.com/dialog/oauth';
+    }
 
-	public function urlAccessToken()
-	{
-		return 'https://graph.facebook.com/oauth/access_token';
-	}
+    public function urlAccessToken()
+    {
+        return 'https://graph.facebook.com/oauth/access_token';
+    }
 
-	public function urlUserDetails(\Oauth2\Client\Token\Access $token)
-	{
-		return 'https://graph.facebook.com/me?'.http_build_query(array(
-			'access_token' => (string) $token,
-		));
-	}
+    public function urlUserDetails(\Oauth2\Client\Token\Access $token)
+    {
+        return 'https://graph.facebook.com/me?'.http_build_query(array(
+            'access_token' => (string) $token,
+        ));
+    }
 
-	public function userDetails($response, \Oauth2\Client\Token\Access $token)
-	{
-		$details = array(
-			'uid' => $response->id,
-			'nickname' => isset($response->username) ? $response->username : null,
-			'name' => $response->name,
-			'first_name' => $response->first_name,
-			'last_name' => $response->last_name,
-			'email' => isset($response->email) ? $response->email : null,
-			'location' => isset($response->hometown->name) ? $response->hometown->name : null,
-			'description' => isset($response->bio) ? $response->bio : null,
-			'urls' => array(
-			  'Facebook' => $response->link,
-			),
-			'image'	=>	null
-		);
+    public function userDetails($response, \Oauth2\Client\Token\Access $token)
+    {
+        $details = array(
+            'uid' => $response->id,
+            'nickname' => isset($response->username) ? $response->username : null,
+            'name' => $response->name,
+            'first_name' => $response->first_name,
+            'last_name' => $response->last_name,
+            'email' => isset($response->email) ? $response->email : null,
+            'location' => isset($response->hometown->name) ? $response->hometown->name : null,
+            'description' => isset($response->bio) ? $response->bio : null,
+            'urls' => array(
+              'Facebook' => $response->link,
+            ),
+            'image' =>  null
+        );
 
-		if ($headers = get_headers('https://graph.facebook.com/me/picture?type=normal&access_token='.$token, 1))
-		{
-			$details['image'] = $headers['Location'];
-		}
+        if ($headers = get_headers('https://graph.facebook.com/me/picture?type=normal&access_token='.$token, 1))
+        {
+            $details['image'] = $headers['Location'];
+        }
 
-		return $details;
-	}
+        return $details;
+    }
 }
